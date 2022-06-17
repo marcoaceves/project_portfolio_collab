fa5ce100404338e3c7ad7c4c87aec32886352389
Marco Aceves <85464208+marcoaceves@users.noreply.github.com>
2022-06-16 22:03:56 -0700

    users models and dashboard created

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,89 @@
+from flask import Flask, flash, request, redirect, session, url_for, render_template
+import os
+from werkzeug.utils import secure_filename
+import urllib.request
+from datetime import datetime
+from flask_app.models.image import Image
+from flask_bcrypt import Bcrypt
+from flask_app import app
+import uuid as uuid
+from flask_app.models.user import User
+
+bcrypt = Bcrypt(app)
+
+# image proccessing
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'avif'])
+UPLOAD_FOLDER = 'flask_app/static/uploads'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1080 * 1554
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# -----------------------------------------------------------------
+
+
+@app.route('/register',methods=["POST","GET"])
+def display_registration():
+
+    return render_template('registration.html', images=Image.get_all_images())
+
+
+@app.route('/register/query', methods=['POST'])
+def query_registration():
+    # if not User.validate_user(request.form):
+    #     return redirect(request.referrer)
+    # files = request.files.getlist('files[]')
+    # print(files)
+    # pic_name=''
+    # for file in files:
+    #     print("hello")
+    #     if file and allowed_file(file.filename):
+    #         filename = secure_filename(file.filename)
+    #         pic_name = str(uuid.uuid1()) + "_" + filename
+    #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], pic_name))
+    # print(pic_name,'pic_name')
+
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+
+    data={
+        'first_name' : request.form['first_name'],
+        'last_name' : request.form['last_name'],
+        'image' : 'default.png',
+        'email' : request.form['email'],
+        'password' : pw_hash,
+        'confirm_password' : request.form['confirm_password'],
+    }
+    User.create_user(data)
+    return redirect('/login')
+
+@app.route('/login',methods=["POST","GET"])
+def display_login():
+    if 'user_id' in session:
+        return redirect('/dashboard')
+
+    return render_template('login.html', images=Image.get_all_images())
+@app.route('/login/query', methods=['POST'])
+def login():
+    data={'email' : request.form['email']}
+    user_in_db = User.get_by_email(data)
+    if not user_in_db:
+        flash('Invalid Email or Password!', 'login')
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash('Invalid Email or Password!', 'login')
+        return redirect('/login')
+    session['user_id']  = user_in_db.id
+
+    return redirect ('/dashboard')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect('/login')
+    data={'id':session["user_id"]}
+    user=User.get_user(data)
+    return render_template('dashboard.html', user=user)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
---
+++ flask_app/models/user.py
@@ -0,0 +1,73 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+import re
+from flask_app import app
+from flask import flash
+from flask_bcrypt import Bcrypt
+bcrypt =Bcrypt(app)
+
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+PASS_REGEX = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$")
+
+
+db ='project_manager_db'
+
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.image = data['image']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def create_user(cls, data):
+        query = 'INSERT INTO users (first_name, last_name, image,  email, password) VALUES (%(first_name)s, %(last_name)s,%(image)s,%(email)s, %(password)s);'
+        return connectToMySQL(db).query_db(query, data)
+
+    @classmethod
+    def get_by_email(cls, data):
+        query ="SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL(db).query_db(query, data)
+        if len(result)< 1 :
+            return False
+        print(result)
+        return cls(result[0])
+    @classmethod
+    def get_user(cls, data):
+        query = "SELECT first_name, last_name, image, email, created_at, updated_at, id FROM users WHERE id = %(id)s"
+        result = connectToMySQL(db).query_db(query, data)
+        print(result)
+        result[0]['password'] = ''
+        return cls(result[0])
+
+    @staticmethod
+    def validate_user( user ):
+        is_valid = True
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        results = connectToMySQL(db).query_db(query,user)
+        query = "SELECT * FROM users WHERE user_name = %(user_name)s;"
+        resultsUsername = connectToMySQL(db).query_db(query,user)
+        # test whether a field matches the pattern
+        if len(results) >= 1:
+            flash("Email already taken.","register")
+            is_valid=False
+        if len(resultsUsername) >= 1:
+            flash("User Name is already taken.","register")
+            is_valid=False
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Invalid Email!!!","register")
+            is_valid=False
+        if not PASS_REGEX.match(user['password']):
+            flash("Password must contain at least 8 characters, 1 Uppercase, 1 Lowercase, 1 Number","register")
+            is_valid=False
+        if len(user['user_name']) < 3:
+            flash("User name must be at least 3 characters","register")
+            is_valid= False
+
+        if user['password'] != user['confirm_password']:
+            flash("Passwords don't match","register")
+        return is_valid
