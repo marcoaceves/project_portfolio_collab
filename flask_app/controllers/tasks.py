5ea7a10e0891b6a0494bf2dcefd06d663148da5b
Dayry Viana <dayry.viana@gmail.com>
2022-06-21 22:21:13 -0400

    Changes

---
+++ flask_app/controllers/tasks.py
@@ -0,0 +1,13 @@
+from flask import Flask, flash, request, redirect, url_for, render_template
+import os
+from werkzeug.utils import secure_filename
+import urllib.request
+from datetime import datetime
+from flask_app.models.image import Image
+from flask_app import app
+import uuid as uuid
+
+@app.route('/display/tasks',methods=["POST","GET"])
+def display_tasks():
+
+    return render_template('open_tasks.html')
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask_app.controllers import images, projects, users
+from flask_app.controllers import images, projects, users,tasks
 from flask_app import app
 
 if __name__ == "__main__":
